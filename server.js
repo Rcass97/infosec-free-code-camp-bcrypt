a804af7221acbc7d4999c13a720072ab7f45f8c6
Reed Cass <98681839+Rcass97@users.noreply.github.com>
2024-01-23 21:09:16 -0700

    Lesson 2, Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash('passw0rd!', 13, (err, hash) => {
+
+bcrypt.hash('myCr@zyP@ssw0rd!', 13, (err, hash) => {
     console.log(hash);
-    //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
-    bcrypt.compare('passw0rd!', hash, (err, res) => {
+    bcrypt.compare('myCr@zyP@ssw0rd!', hash, (err, res) => {
       console.log(res); //true
     });
   });
